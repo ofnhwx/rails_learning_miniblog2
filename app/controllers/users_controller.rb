bdf7a504d2cda5aee6a8d4ffc7e8f02c6f7a08d6
Yuta Fujita <ofnhwx@komunan.net>
2020-04-11 23:26:17 +0900

    ユーザーページをでっちあげ

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  def show; end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/show'
   root 'root#index'
+
   devise_for :users
+  resources :users, only: [:show]
 end
