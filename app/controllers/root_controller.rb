19e0ed51e2da2dc49941ca87a1297634dcca4fa0
Yuta Fujita <ofnhwx@komunan.net>
2020-04-11 23:14:48 +0900

    トップページでの投稿一覧表示を実装

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 class RootController < ApplicationController
+  include Pagy::Backend
+
   def index
+    @pagy, @posts = pagy(Post.all)
   end
 end
---
+++ config/initializers/pagy.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+Pagy::VARS[:items] = 10
+require 'pagy/extras/bootstrap'
