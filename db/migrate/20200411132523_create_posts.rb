059cb4dd4b37b56e8893e686cc7d9e065027c1dd
Yuta Fujita <ofnhwx@komunan.net>
2020-04-11 22:30:53 +0900

    投稿用のテーブルを追加

---
+++ db/migrate/20200411132523_create_posts.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.text :content
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+
+    add_index :posts, %i[user_id create_at]
+  end
+end
