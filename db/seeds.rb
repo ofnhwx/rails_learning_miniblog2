90f47a30c5452d5cd820d26ad81bb589f687cf75
Yuta Fujita <ofnhwx@komunan.net>
2020-04-11 22:43:53 +0900

    Faker を使用して初期データを投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+User.create!(email: 'user@example.com', password: 'password')
+
+10.times do
+  User.create!(email: Faker::Internet.email, password: 'password')
+end
+
+users = User.order(:create_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.posts.create!(content: content) }
+end
