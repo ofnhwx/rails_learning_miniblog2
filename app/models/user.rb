b5496e776c593155c8711ba17d86765a966f93b8
Yuta Fujita <ofnhwx@komunan.net>
2020-04-14 20:52:51 +0900

    フォロー, アンフォローの準備

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   def like(post)
     liked_posts << post unless like? post
@@ -20,5 +24,17 @@ class User < ApplicationRecord
 
   def like?(post)
     liked_posts.include? post
+  end
+
+  def follow(other_user)
+    following << other_user unless following? other_user
+  end
+
+  def unfollow(other_user)
+    following.destroy other_user
+  end
+
+  def following?(other_user)
+    following.include? other_user
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_124708) do
+ActiveRecord::Schema.define(version: 2020_04_14_114232) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 2020_04_13_124708) do
     t.datetime "updated_at", precision: 6, null: false
     t.index "\"user_id\", \"create_at\"", name: "index_posts_on_user_id_and_create_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
